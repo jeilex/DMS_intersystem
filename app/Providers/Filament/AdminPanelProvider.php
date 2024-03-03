43cb50b3be18bb19dd04b02099346141e5fd4dd2
jeilex <jeilex1130@gmail.com>
2024-03-04 00:05:54 +0800

    logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,10 +32,10 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->unsavedChangesAlerts()
-            ->brandLogo(asset('images/bg.jpg')) 
+            ->brandLogo(asset('images/intersys.jpg')) 
             // ->brandName('Document Management System')
             // ->font('Arial')
-            ->favicon(asset('images/bg.jpg'))
+            ->favicon(asset('images/intersys.jpg'))
             ->brandLogoHeight('50px')
             ->colors([
                 'primary' => Color::Amber,
